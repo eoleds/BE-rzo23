2a08948049ee1021f92b13730061eaef5bca2f7a
st4rhub <oceanepioche@gmail.com>
2023-04-21 11:57:50 +0200

    phase d'établissement de connexion

--- src/mictcp.c
+++ src/mictcp.c
@@ -8,11 +8,12 @@
 int mic_tcp_socket(start_mode sm)
 {
    printf("[MIC-TCP] Appel de la fonction: ");  printf(__FUNCTION__); printf("\n");
+   int result = -1;
    mic_tcp_sock sock;
    sock.fd = 0;
-   initialize_components(sm); /* Appel obligatoire */
+   result = initialize_components(sm); /* Appel obligatoire */
    set_loss_rate(0);
-   return 0;
+   return result;
 }
 
 /*
@@ -26,10 +27,11 @@ int mic_tcp_bind(int socket, mic_tcp_sock_addr addr)
 }
 
 /*
- * Met le socket en état d'acceptation de connexions
+ * Met le socket en état d'acceptation de connexion
  * Retourne 0 si succès, -1 si erreur
  */
 
+//Variable booléenne pour savoir si la connexion est bien établie
 int connexion_etablie = 0;
 
 int mic_tcp_accept(int socket, mic_tcp_sock_addr* addr)
@@ -38,8 +40,6 @@ int mic_tcp_accept(int socket, mic_tcp_sock_addr* addr)
     while(!connexion_etablie){   
         //attente de l'etablissement de connexion
     }
-    mic_tcp_pdu rec;
-    IP_recv(&rec, addr, 1); //Attente de reception de l'ACK
     return 0;
 }
 
@@ -47,27 +47,56 @@ int mic_tcp_accept(int socket, mic_tcp_sock_addr* addr)
  * Permet de réclamer l’établissement d’une connexion
  * Retourne 0 si la connexion est établie, et -1 en cas d’échec
  */
-int pe = 0; // on définit le prochain paquet à emettre
+
+// on définit le prochain paquet à emettre (PE)
+int pe = 0;
 int mic_tcp_connect(int socket, mic_tcp_sock_addr addr)
 {
     printf("[MIC-TCP] Appel de la fonction: ");  printf(__FUNCTION__); printf("\n");
-    //IP send et receive
-    return 0;
-    mic_tcp_pdu pdu;
-    //remplir le header
-    pdu.header.syn = '1';
+    
+    // On déclare + initialise le PDU SYN
+    mic_tcp_pdu pdu = {0};
+    pdu.header.syn = 1;
+    pdu.header.ack = 0;
+
+    // On associe puis update le numéro du prochain paquet à émettre
     pdu.header.seq_num = pe;
+    pe = (pe+1)%2;
 
-    mic_tcp_pdu rec; //pdu qui va recevoir
+    pdu.payload.data = "";
+    pdu.payload.size = 0;
 
-    //activation timer
-    int result = IP_send(pdu, addr);
-    int time = IP_recv(&rec, &addr, 10);
-    while (time == -1){
+    mic_tcp_pdu rec = {0}; //pdu qui va recevoir
+
+
+    int result;
+    int time_;
+
+    //Variable booléenne pour savoir si on a reçu le SYN_ACK qu'on attend
+    int syn_ack_recu = 0;
+
+    while (syn_ack_recu == 0){
         result = IP_send(pdu, addr);
-        time = IP_recv(&rec, &addr, 1000);
+        time_ = IP_recv(&rec, &addr, 10);
+        //Si le timer n'a pas expiré et que le PDU recu est bien un SYN_ACK
+        if (time_ != -1 && rec.header.syn == 1 && rec.header.ack == 1){
+            //On déclare qu'on l'a bien reçu pour sortir de la boucle
+            syn_ack_recu = 1;
+        }
     }
-    return result;
+
+    //On initialise l'ACK à envoyer
+    mic_tcp_pdu ACK = {0};
+    ACK.header.syn = 0;
+    ACK.header.ack = 1;
+    ACK.header.seq_num = pe;
+    ACK.payload.data = "";
+    ACK.payload.size = 0;
+
+    // On envoie l'ACK
+    IP_send(ACK, addr);
+    printf("Connexion établie\n");
+    return 0;
 }
 
 /*
@@ -86,22 +115,22 @@ int mic_tcp_send (int mic_sock, char* mesg, int mesg_size)
 {
     printf("[MIC-TCP] Appel de la fonction: "); printf(__FUNCTION__); printf("\n");
         mic_tcp_pdu pdu;
-
         //remplir le payload
         pdu.payload.size = mesg_size;
         pdu.payload.data = mesg;
         pdu.header.seq_num = pe; // DT.nseq <-- pe
 
-        mic_tcp_pdu pk; //PDU de l'ACK à recevoir
-        mic_tcp_sock_addr addr_pk;
+        //PDU de l'ACK à recevoir
+        mic_tcp_pdu pk = {0};
+        mic_tcp_sock_addr addr = {0}, addr_pk = {0};
 
-        addr_pk.ip_addr = "localhost";
-        addr_pk.ip_addr_size = strlen(addr_pk.ip_addr) + 1;
-        addr_pk.port = htons(API_CS_Port);
+        addr.ip_addr = "localhost";
+        addr.ip_addr_size = strlen(addr.ip_addr) + 1;
+        addr.port = htons(API_CS_Port);
 
         // activation timer
-        int env = IP_send(pdu,addr_pk);
-        int rec = IP_recv(&pk, &addr_pk, 10);
+        int env ;//= IP_send(pdu,addr_pk);
+        int rec ;//= IP_recv(&pk, &addr_pk, 10);
 
         //On s'apprête à envoyer un paquet
         paquets_env++;
@@ -109,11 +138,11 @@ int mic_tcp_send (int mic_sock, char* mesg, int mesg_size)
         pe = (pe+1)%2;
         int envoi_a_faire = 1;
         while (envoi_a_faire){
-            env = IP_send(pdu,addr_pk);
+            env = IP_send(pdu,addr);
             //On considère d'abord l'envoi réussi
             envoi_a_faire = 0;
             //On attend de recevoir un ack
-            rec = IP_recv(&pk, &addr_pk, 1);
+            rec = IP_recv(&pk, &addr_pk, 10);
 
             //Si on n'a rien rçu :
             if(rec == -1){
@@ -146,12 +175,14 @@ int mic_tcp_send (int mic_sock, char* mesg, int mesg_size)
  * NB : cette fonction fait appel à la fonction app_buffer_get()
  */
 
+// On définit le prochain paquet attendu (PA)
 int pa = 0;
 
 int mic_tcp_recv (int socket, char* mesg, int max_mesg_size)
 {
     printf("[MIC-TCP] Appel de la fonction: "); printf(__FUNCTION__); printf("\n");
     int result = -1;
+    //On crée un PDU qu'on remplit des informations qu'on a reçues (data + size)
     mic_tcp_pdu pdu;
     pdu.payload.size = max_mesg_size;
     pdu.payload.data = mesg;
@@ -182,18 +213,35 @@ int mic_tcp_close (int socket)
 void process_received_PDU(mic_tcp_pdu pdu, mic_tcp_sock_addr addr)
 {
     printf("[MIC-TCP] Appel de la fonction: "); printf(__FUNCTION__); printf("\n");
-    mic_tcp_pdu ACK_PDU;
-    ACK_PDU.header.ack = '1';
-    if (pdu.header.seq_num == pa){
+    mic_tcp_pdu ACK_PDU = {0};
+    ACK_PDU.header.ack = 1;
+    //On traite le cas de reception d'un SYN (-> envoi d'un SYN_ACK)
+    if (pdu.header.syn == 1 && pdu.header.ack == 0){
+        // On créé un PDU SYN_ACK qu'on initialise
+        mic_tcp_pdu SYN_ACK_PDU = {0};
+        SYN_ACK_PDU.header.ack = 1;
+        SYN_ACK_PDU.header.syn = 1;
+        SYN_ACK_PDU.payload.data = "";
+        SYN_ACK_PDU.payload.size = 0;
+
+        // On update le num du prochain paquet a emettre
+        pa = (pa + 1)%2;
+        SYN_ACK_PDU.header.seq_num = pa;
+
+        // On envoie le SYN_ACK
+        IP_send(SYN_ACK_PDU, addr);
+
+    // Le cas de reception ACK (-> etablissement de connexion)
+    }else if (pdu.header.syn == 0 && pdu.header.ack == 1){
+        connexion_etablie = 1;
+
+    // Le cas de reception d'une data (-> update de PA + envoi ACK)
+    }else if (pdu.header.seq_num == pa){
         app_buffer_put(pdu.payload);
         pa = (pa + 1)%2;
         ACK_PDU.header.ack_num = pa;
         IP_send(ACK_PDU, addr);
-        if (pdu.header.syn == '1'){
-            connexion_etablie = 1;
-        }
-    }
-    else{
+    }else{
         ACK_PDU.header.ack_num = pa;
         IP_send(ACK_PDU, addr);
     }
